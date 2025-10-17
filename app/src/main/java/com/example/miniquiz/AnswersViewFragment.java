19b75454a36e23f4554220c0e310417ef1259135
krol.bartosz <krol.bartosz@zst.radom.pl>
2025-10-17 10:34:22 +0200

    nwm błąd

---
+++ app/src/main/java/com/example/miniquiz/AnswersViewFragment.java
@@ -0,0 +1,9 @@
+package com.example.miniquiz;
+
+import androidx.fragment.app.Fragment;
+
+public class AnswersViewFragment extends Fragment {
+    public AnswersViewFragment(){
+        super(R.layout.answers_view);
+    }
+}
--- app/src/main/java/com/example/miniquiz/QuizViewFragment.java
+++ app/src/main/java/com/example/miniquiz/QuizViewFragment.java
@@ -1,9 +1,29 @@
 package com.example.miniquiz;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.fragment.app.Fragment;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class QuizViewFragment extends Fragment {
     public QuizViewFragment() {
         super(R.layout.quiz_view);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (savedInstanceState == null) {
+            getChildFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, StartViewFragment.class, null)
+                    .commit();
+        }
     }
 }
