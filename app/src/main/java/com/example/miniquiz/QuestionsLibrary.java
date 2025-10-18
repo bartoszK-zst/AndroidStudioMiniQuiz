3ca6623d69931472bb550c8f1bd77bf95b1a5ffe
bartoszK-zst <krol.bartosz@zst.radom.pl>
2025-10-18 23:48:18 +0200

    to zaszło za daleko

--- app/src/main/java/com/example/miniquiz/QuestionsLibrary.java
+++ app/src/main/java/com/example/miniquiz/QuestionsLibrary.java
@@ -8,6 +8,13 @@ public class QuestionsLibrary {
     private boolean markUsedQuestions = false;
     private boolean[] usedQuestions;
     private int previosQuestionIndex = -1;
+
+    private static final Question[] DEFAULT_QUESTIONS = new Question[] {
+            new Question("Jaki jest stolica Polski?", new String[]{"Warszawa", "Kraków", "Gdańsk"}, 0),
+            new Question("Ile wynosi 2 + 2?", new String[]{"3", "4", "5"}, 1),
+            new Question("Jaki jest największy ocean na Ziemi?", new String[]{"Atlantycki", "Spokojny", "Indyjski"}, 1),
+            new Question("Jaki jest symbol chemiczny wody?", new String[]{"H2O", "CO2", "O2"}, 0)
+    };
     public QuestionsLibrary(Question[] questions, boolean markUsedQuestions){
         this.questions = questions.clone();
         this.markUsedQuestions = markUsedQuestions;
@@ -20,22 +27,23 @@ public class QuestionsLibrary {
         this(questions, true);
     }
     public QuestionsLibrary() {
-        this(new Question[] {
-                new Question("Jaki jest stolica Polski?", new String[]{"Warszawa", "Kraków", "Gdańsk"}, 0),
-                new Question("Ile wynosi 2 + 2?", new String[]{"3", "4", "5"}, 1),
-                new Question("Jaki jest największy ocean na Ziemi?", new String[]{"Atlantycki", "Spokojny", "Indyjski"}, 1),
-                new Question("Jaki jest symbol chemiczny wody?", new String[]{"H2O", "CO2", "O2"}, 0)
-        });
+        this(DEFAULT_QUESTIONS);
     }
     public QuestionsLibrary(boolean markUsedQuestions) {
-        this();
-        this.markUsedQuestions = markUsedQuestions;
-        if(markUsedQuestions) {
-            usedQuestions = new boolean[questions.length];
-            markQuestionsUnused();
+        this(DEFAULT_QUESTIONS, markUsedQuestions);
+    }
+    public Question getQuestion(int index, boolean markQuestionUsed, boolean acceptSameQuestionInRow){
+        if(index < 0 || index >= questions.length) {
+            return null;
+        }
+        if(!acceptSameQuestionInRow && index == previosQuestionIndex) {
+            getQuestion(markQuestionUsed);
+        }
+        else{
+            return questions[index];
         }
     }
-    public Question getQuestion(int index) {
+    public Question getQuestion(int index, boolean markQuestionUsed) {
         if (index >= 0 && index < questions.length) {
             Question q = questions[index];
             if (markUsedQuestions) {
@@ -45,21 +53,29 @@ public class QuestionsLibrary {
                 }
                 usedQuestions[index] = true;
             }
+            if(markQuestionUsed){
+                usedQuestions[index] = true;
+            }
             previosQuestionIndex = index;
             return q;
         }
         return null;
     }
-    public Question getQuestion(){
-        if(markUsedQuestions) {
-            // upewnij się, że tablica użytych pytań jest zainicjalizowana
-            if (usedQuestions == null) {
-                markQuestionsUnused();
+    public Question getQuestion(int index) {
+        return getQuestion(index, markUsedQuestions);
+    }
+    public Question getQuestion(boolean includeUsed, boolean markQuestionUsed){
+        Random Rand = new Random();
+        int questionIndex = Rand.nextInt(questions.length);
+        if(includeUsed){
+            if(markQuestionUsed){
+                usedQuestions[questionIndex] = true;
             }
-             Random rand = new Random();
-             int questionIndex = rand.nextInt(questions.length);
-             int attempts = 0;
-             while(usedQuestions[questionIndex] || questionIndex == previosQuestionIndex) {
+            return questions[questionIndex];
+        }
+        else{
+            int attempts = 0;
+            while(usedQuestions[questionIndex] || questionIndex == previosQuestionIndex) {
                 attempts++;
                 if(questionIndex == questions.length - 1) {
                     questionIndex = 0;
@@ -67,18 +83,24 @@ public class QuestionsLibrary {
                     questionIndex++;
                 }
 
-                //Jeśli wszystkie pytania zostały użyte, resetuje stan użycia pytań
                 if(attempts >= questions.length) {
                     markQuestionsUnused();
-                    return getQuestion();
+                    return getQuestion(true, markQuestionUsed);
                 }
             }
-
-            //Jeśli znaleziono nieużyte pytanie, zwraca je i oznacza jako użyte
-            return getQuestion(questionIndex);
+            if(markQuestionUsed){
+                usedQuestions[questionIndex] = true;
+            }
+            return questions[questionIndex];
         }
-        return getQuestion(0);
     }
+    public Question getQuestion(boolean manageUsedQuestions) {
+        return getQuestion(manageUsedQuestions, manageUsedQuestions);
+    }
+    public Question getQuestion(){
+        return getQuestion(markUsedQuestions);
+    }
+
     public int getTotalQuestions() {
         return questions.length;
     }
